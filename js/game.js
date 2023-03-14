51708dd77f0ab7af1c0fb36548690931649ad46e
José Antonio Escudero García <jaeg90@gmail.com>
2023-03-14 18:41:37 +0100

    Background added

--- js/game.js
+++ js/game.js
@@ -8,7 +8,7 @@ const Game = {
   background: undefined,
   player: undefined,
   score: undefined,
-  obstacles: [],
+  chainsaws: [],
 
   keys: {
     SPACE: 32,
@@ -31,13 +31,42 @@ const Game = {
     this.canvas.setAttribute("height", this.height);
   },
 
-  start() {},
+  start() {
+    this.reset();
+    this.interval = setInterval(() => {
+      this.framesCounter++;
+      if (this.framesCounter > 3000) {
+        this.framesCounter = 0;
+      }
 
-  reset() {},
+      //1. Clear canvas
+      this.clear();
+      //2. Dran elements
+      this.drawAll();
+      //3. Move chainsaws
+      //4. Clear chainsaws
+      //5. Check if isCollision and invoke .gameOver
+    }, 1000 / this.FPS);
+  },
 
-  drawAll() {},
+  reset() {
+    //1. Create background
+    this.background = new Background(this.ctx, this.width, this.height);
+    //2. Create player
+    //3. Create chainsaw array
+    this.chainsaws = [];
+  },
 
-  clear() {},
+  drawAll() {
+    //1. Draw background
+    this.background.draw();
+    //2. Draw player
+    //3. Draw chainsaws array
+  },
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  },
 
   isCollistion() {},
 
