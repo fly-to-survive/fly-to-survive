b2b5bc52c8d10ad547b4bcf34e57e698c070eaee
José Antonio Escudero García <jaeg90@gmail.com>
2023-03-22 20:59:18 +0100

    Borders fixed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
   this.gameOn = false;
+  this.canvas = document.querySelector("#myCanvas");
   document.addEventListener("keydown", (e) => {
     if (e.keyCode == 32 && !gameOn) {
       Game.init();
@@ -7,6 +8,7 @@ window.onload = () => {
     }
     if (e.keyCode == 82 && gameOn) {
       Game.init();
+      canvas.style.border = "6px solid rgb(165, 99, 42)";
     }
   });
 };
